c53794d90eb5d99bfea87fb9202a35c04cfa7ee6
Paulo301 <paulovictor1918@gmail.com>
2023-07-13 21:38:00 -0300

    Implementando Dialog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,9 @@ import LogoImg from '../assets/logo.svg';
 
 import styles from '../styles/Home.module.css';
 import Head from "next/head";
-import { useEffect, useRef, useState } from "react";
+import * as Dialog from '@radix-ui/react-dialog';
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const modalRef = useRef<HTMLDivElement | null>(null);
-
-  function handleModalOpen() {
-    setIsModalOpen(true);
-  }
-
-  useEffect(() => {
-    if(isModalOpen) {
-      modalRef?.current?.focus();
-    }
-  }, [isModalOpen]);
 
   return (
     <>
@@ -55,26 +43,29 @@ export default function Home() {
         <Image src={LogoImg} width={286 / 2} alt='Blog da Rocketseat' />
 
         <nav className={styles.nav} aria-label='footer'>
-          <button type="button" onClick={handleModalOpen} aria-controls='modal1'>
-            Termos de uso
-          </button>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <button type="button">
+                Termos de uso
+              </button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className={styles.overlay} />
+
+              <Dialog.Content className={styles.modal}>
+                <Dialog.Title>Termos de uso</Dialog.Title>
+                <Dialog.Description>Essses são os termos de uso</Dialog.Description>
+                <Dialog.Close asChild>
+                  <button className={styles.closeModalButton}>
+                    Fechar
+                  </button>
+                </Dialog.Close>
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </nav>
       </footer>
-
-      {isModalOpen && (
-        <div
-          id='modal1'
-          ref={modalRef}
-          className={styles.modal} 
-          role='dialog' 
-          aria-labelledby='modal1Title'
-          aria-describedby='modal1Description'
-          tabIndex={-1}
-        >
-          <h2 id='modal1Title'>Termos de uso</h2>
-          <p id='modal1Description'>Essses são os termos de uso</p>
-        </div>
-      )}
     </>
   )
 }
